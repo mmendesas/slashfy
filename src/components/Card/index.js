d02c8e8e3e67edd820570c0cf1e18613332db84d
Marcio Mendes <mmendes.as@gmail.com>
2019-12-04 01:41:36 -0300

    feat: add context to board to move cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,13 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
+
+import BoardContext from '../Board/context';
 
 import { Container, Label } from './styles';
 
 export default function Card({ data, index }) {
   const ref = useRef();
+  const { move } = useContext(BoardContext);
 
   const [{ isDragging }, dragRef] = useDrag({
     item: { type: 'CARD', index },
@@ -36,6 +39,8 @@ export default function Card({ data, index }) {
       if (draggedIndex > targetIndex && draggedTop > targetCenter) {
         return;
       }
+
+      move(draggedIndex, targetIndex);
     }
   });
 
