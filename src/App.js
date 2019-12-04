5de18c0509a626b064ce99d8f339fa5bf139a141
Marcio Mendes <mmendes.as@gmail.com>
2019-12-03 23:50:40 -0300

    chore: setup global style with styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
-    <div className="App">
+    <GlobalStyle>
       <h1>Hello World</h1>
-    </div>
+    </GlobalStyle>
   );
 }
 
